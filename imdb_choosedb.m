e89b15cef34a3d26849b64d2c8c166e7f993bc13
Alex Dewar <alex.dewar@gmx.co.uk>
2016-12-05 15:08:29 +0000

    update choosedb code

--- imdb_choosedb.m
+++ imdb_choosedb.m
@@ -1,10 +1,21 @@
-function whd=imdb_choosedb
+function [whd,whdshort,label]=imdb_choosedb(is3d)
+if ~nargin
+    is3d = false;
+end
 
-d = dir(fullfile(mfiledir,'imdb_*'));
+ddir = fullfile(mfiledir,'data');
+if is3d
+    d = dir(fullfile(ddir,'unwrap_imdb3d_*'));
+else
+    d = dir(fullfile(ddir,'unwrap_imdb_*'));
+end
+
 ds = {};
+labels = {};
 for i = 1:length(d)
     if d(i).isdir
         ds{end+1} = d(i).name;
+        labels{end+1} = imdb_getlabel(fullfile(ddir,d(i).name));
     end
 end
 
@@ -13,17 +24,24 @@ switch length(ds)
         disp('no image dbs found')
         return
     case 1
-        whd = ds{1};
+        whdshort = ds{1};
+        label = labels{1};
     otherwise
         for i = 1:length(ds)
-            fprintf('%3d. %s\n',i,ds{i});
+            if isempty(labels{i})
+                fprintf('%3d. %s\n',i,ds{i});
+            else
+                fprintf('%3d. %s (%s)\n',i,ds{i},labels{i});
+            end
         end
         inp = input(sprintf('Which dir [%d]: ',length(ds)));
-        if isempty(inp);
-            whd = ds{end};
+        if isempty(inp)
+            whdshort = ds{end};
+            label = labels{end};
         else
-            whd = ds{inp};
+            whdshort = ds{inp};
+            label = labels{inp};
         end
 end
 
-whd = fullfile(mfiledir,whd);
+whd = fullfile(mfiledir,whdshort);
