a484e910a9e4d951ed84a4bf6d84c1048877222e
Alex Dewar <alex.dewar@gmx.co.uk>
2016-12-21 15:09:52 +0000

    tweak auto-update code

--- stable/update_alex.m
+++ stable/update_alex.m
@@ -1,5 +1,7 @@
 function update_alex
 [stat,res] = system(sprintf('git -C "%s" pull', mfiledir));
-if stat ~= 0
+if stat == 0
+    disp(res)
+else
     warning('code failed to update: %s', res);
 end
