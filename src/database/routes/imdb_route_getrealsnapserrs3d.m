766c756f63311ede351d1f49b01bf27abd802cf5
Alex Dewar <a.dewar@sussex.ac.uk>
2017-02-16 17:10:10 +0000

    print target fn

--- src/database/routes/imdb_route_getrealsnapserrs3d.m
+++ src/database/routes/imdb_route_getrealsnapserrs3d.m
@@ -16,6 +16,7 @@ else
 end
 figdatfn = fullfile(imdb_route_figdatdir,sprintf('imdb3d_route_geterrs_%s_%s_%03d_res%03d_z%d%s.mat',shortwhd,matfileremext(arenafn),routenum,res,zht,infomaxstr));
 
+fprintf('target file: %s\n',figdatfn);
 isnew = ~exist(figdatfn,'file');
 if ~isnew && ~forcegen
     load(figdatfn);
