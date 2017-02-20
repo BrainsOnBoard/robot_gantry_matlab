5e22de31d58cb9087c9528e995f31c57085b81bb
Alex Dewar <alex.dewar@gmx.co.uk>
2017-02-20 21:31:24 +0000

    - fix ranking bug - new fig to get rank of "nearest" snap

--- src/database/routes/imdb_route_rank_nearest.m
+++ src/database/routes/imdb_route_rank_nearest.m
@@ -13,14 +13,12 @@ hold on
 for czht = zht
     [imxi,imyi,heads,whsn,err,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn] = imdb_route_getrealsnapserrs3d(shortwhd,'arena2_pile',routenum,res,czht,false,forcegen);
     
-    rankscore = NaN(size(allwhsn,2),1);
-    for i = 1:length(rankscore)
-        ranks = NaN(size(allwhsn,2),1);
-        for j = 1:size(allwhsn,1)
-            ranks(j) = find(allwhsn(j,:)==i);
-        end
-        rankscore(i) = size(allwhsn,2) - mean(ranks);
+    ranks = NaN(numel(nearest),1);
+    for i = 1:length(ranks)
+        ranks(i) = find(allwhsn(i,:)==nearest(i));
     end
-    plot(rankscore)
-    xlim([1 length(rankscore)])
+    plot(ranks)
+    xlim([1 length(nearest)])
+    
+    return
 end
