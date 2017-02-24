9339ac00594e10b1b9469ad2de46bf6035e78824
Alex Dewar <alex.dewar@gmx.co.uk>
2017-02-24 17:14:25 +0000

    "allow for using binarised snaps for infomax training cf. histeq"

--- src/routes/gantry_rf_getinfomaxweightsfov.m
+++ src/routes/gantry_rf_getinfomaxweightsfov.m
@@ -1,55 +1,49 @@
-function gantry_rf_getinfomaxweightsfov(imw,dohisteq)
+function gantry_rf_getinfomaxweightsfov(imw,improc)
 if nargin < 2
-    dohisteq = true;
+    improc = 'histeq_';
+else
+    improc = [improc, '_'];
 end
 if nargin < 1
     imw = 90;
 end
 
-for cdohisteq = dohisteq(:)'
-    for cimw = imw(:)'
-        if cdohisteq
-            histeqstr = 'histeq_';
-        else
-            histeqstr = '';
-        end
+for cimw = imw(:)'
+    flist = dir(fullfile(routes_fovsnapdir,sprintf('%ssnaps_*_imw%03d.mat',improc,cimw)));
+    for i = 1:length(flist)
+        cfn = flist(i).name;
         
-        flist = dir(fullfile(routes_fovsnapdir,sprintf('%ssnaps_*_imw%03d.mat',histeqstr,cimw)));
-        for i = 1:length(flist)
-            cfn = flist(i).name;
+        fprintf('Loading %s...\n',cfn)
+        
+        datafn = fullfile(routes_fovsnapdir,cfn);
+        outfn = fullfile(routes_infomaxweightsdir,sprintf('infomax_%s.mat',matfileremext(cfn)));
+        
+        if exist(outfn,'file')
+            warning('%s exists. skipping.',outfn)
+        else
+            load(datafn);
+            imsz = [size(fovsnaps,1), size(fovsnaps,2)];
             
-            fprintf('Loading %s...\n',cfn)
+            %         cfov = str2double(cfn(end-6:end-4));
             
-            datafn = fullfile(routes_fovsnapdir,cfn);
-            outfn = fullfile(routes_infomaxweightsdir,sprintf('infomax_%s.mat',matfileremext(cfn)));
-            
-            if exist(outfn,'file')
-                warning('%s exists. skipping.',outfn)
-            else
-                load(datafn);
-                imsz = [size(fovsnaps,1), size(fovsnaps,2)];
+            sz = size(fovsnaps);
+            D = NaN([prod(imsz), sz(3)]);
+            for j = 1:sz(3)
+                cim = im2double(fovsnaps(:,:,j));
+                D(:,j) = cim(:);
                 
-                %         cfov = str2double(cfn(end-6:end-4));
-                
-                sz = size(fovsnaps);
-                D = NaN([prod(imsz), sz(3)]);
-                for j = 1:sz(3)
-                    cim = imresize(im2double(fovsnaps(:,:,j)),imsz,'bilinear');
-                    D(:,j) = cim(:);
-                    
-                    %             figure(1);clf
-                    %             imshow(cim)
-                    %             ginput(1);
-                end
-                
-                disp('Training network...')
-                tic
-                W=infomax_train(size(D,1),D);
-                traint=toc;
-                fprintf('%gs taken to train network\n',traint)
-                
-                savemeta(outfn,'W','imsz','traint');
+                %             figure(1);clf
+                %             imshow(cim)
+                %             ginput(1);
             end
+            
+            disp('Training network...')
+            tic
+            W=infomax_train(size(D,1),D);
+            traint=toc;
+            fprintf('%gs taken to train network\n',traint)
+            
+            savemeta(outfn,'W','imsz','traint');
         end
     end
 end
