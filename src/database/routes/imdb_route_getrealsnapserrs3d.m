5cc6dab9ff8e6e94c7f30db67c03e88e029d0635
Alex Dewar <a.dewar@sussex.ac.uk>
2017-02-28 10:47:38 +0000

    fix: use correct figdat fn with bin or histeq improc method

--- src/database/routes/imdb_route_getrealsnapserrs3d.m
+++ src/database/routes/imdb_route_getrealsnapserrs3d.m
@@ -19,7 +19,12 @@ if useinfomax
 else
     infomaxstr = '';
 end
-figdatfn = fullfile(imdb_route_figdatdir,sprintf('imdb3d_route_geterrs_%s_%s_%03d_res%03d_z%d%s.mat',shortwhd,matfileremext(arenafn),routenum,res,zht,infomaxstr));
+if isempty(improc)
+    improcstr = '';
+else
+    improcstr = [improc,'_'];
+end
+figdatfn = fullfile(imdb_route_figdatdir,sprintf('imdb3d_route_geterrs_%s%s_%s_%03d_res%03d_z%d%s.mat',improcstr,shortwhd,matfileremext(arenafn),routenum,res,zht,infomaxstr));
 
 fprintf('target file: %s\n',figdatfn);
 isnew = ~exist(figdatfn,'file');
