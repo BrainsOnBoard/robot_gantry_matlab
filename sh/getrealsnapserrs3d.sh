173c73b50f0f5832bf7151f39f55ba2e4f5e8c1e
Alex Dewar <a.dewar@sussex.ac.uk>
2017-02-28 10:48:39 +0000

    add (needed) improc parameter to matlab function call

--- sh/getrealsnapserrs3d.sh
+++ sh/getrealsnapserrs3d.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #$ -j y
 #$ -o logs
-#$ -q inf.q,eng-inf_himem.q,greenbrain.q
+#$ -q inf.q,eng-inf_himem.q
 #$ -S /bin/bash
+
+improc=bin
 
 echo running on queue: $QUEUE
 
@@ -13,5 +15,5 @@ echo params: ${params[*]}
 module add matlab
 
 #imdb_route_getrealsnapserrs3d(shortwhd{i},'arena2_pile',routenum,cres,czht,useinfomax,forcegen)
-matlab -nodisplay -singleCompThread -nojvm -r "imdb_route_getrealsnapserrs3d('${params[0]}','${params[1]}',${params[2]},${params[3]},${params[4]},${params[5]},false);exit"
+matlab -nodisplay -singleCompThread -nojvm -r "imdb_route_getrealsnapserrs3d('${params[0]}','${params[1]}',${params[2]},${params[3]},${params[4]},${params[5]},'$improc',false);exit"
 
