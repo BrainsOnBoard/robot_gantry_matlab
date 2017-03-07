e35d6c036c4146736bcea7e433e8b60a5f04a813
Alex Dewar <a.dewar@sussex.ac.uk>
2017-03-07 12:11:09 +0000

    new code to examine histeq vs no preprocessing

---
+++ src/database/routes/imdb_route_histeq_vs_none.m
@@ -0,0 +1,28 @@
+clear
+
+shortwhd={
+    'unwrap_imdb3d_2017-02-09_001'      % open, new boxes
+    %'unwrap_imdb3d_2016-03-23_001', ... % open, empty
+    };
+res = 90;
+zht = 0:100:500;
+routenum = 3;
+forcegen = false;
+showfig = false;
+
+for i = 1:length(zht)
+    [imxi,imyi,heads,whsn,err1,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn] = imdb_route_getrealsnapserrs3d(shortwhd{1},'arena2_pile',routenum,res,zht(i),false,'',forcegen);
+    [imxi,imyi,heads,whsn,err2,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn] = imdb_route_getrealsnapserrs3d(shortwhd{1},'arena2_pile',routenum,res,zht(i),false,'histeq',forcegen);
+    
+    if showfig
+        figure
+        diffim = NaN(length(p.ys),length(p.xs));
+        diffim(sub2ind(size(diffim),imyi,imxi)) = err1 - err2;
+        imagesc(p.xs,p.ys,diffim);
+        colormap hot
+        colorbar;
+        title(sprintf('none minus histeq, ht: %d',zht(i)))
+        set(gca,'YDir','normal')
+        axis equal tight
+    end
+end
---
+++ src/database/routes/imdb_route_histeq_vs_none_compims.m
@@ -0,0 +1,42 @@
+clear
+
+shortwhd = 'unwrap_imdb3d_2017-02-09_001';
+whd = fullfile(imdbdir,shortwhd);
+imsz = [29 360];
+res = 90; % different from imsz because we want the images at a high enough res to see
+zht = 200;
+routenum = 3;
+
+forcegen = false;
+
+load(fullfile(whd,'im_params.mat'),'p')
+
+[imxi,imyi,heads,whsn,err1,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn] = imdb_route_getrealsnapserrs3d(shortwhd,'arena2_pile',routenum,res,zht,false,'',forcegen);
+[imxi,imyi,heads,whsn,err2,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn] = imdb_route_getrealsnapserrs3d(shortwhd,'arena2_pile',routenum,res,zht,false,'histeq',forcegen);
+
+[~,diffsi] = sort(err1 - err2, 'descend');
+
+zi = find(p.zs==zht);
+
+figure(1)
+for i = 1:length(diffsi)
+    clf
+    
+    im = imresize(imdb_getim3d(whd,imxi(diffsi(i)),imyi(diffsi(i)),zi,[]),imsz,'bilinear');
+    imhisteq = histeq(im);
+    
+    alsubplot(3,1,1,1)
+    imshow(im)
+    
+    alsubplot(2,1)
+    imshow(imhisteq)
+    
+    alsubplot(3,1)
+    imagesc(im - imhisteq)
+    
+    colormap gray
+    
+    if isempty(ginput(1))
+        break
+    end
+end
