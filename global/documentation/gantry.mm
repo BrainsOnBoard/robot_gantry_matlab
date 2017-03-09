459fe603ef146338744ceb1d7a289dc45245f034
Alex Dewar <a.dewar@sussex.ac.uk>
2017-03-09 16:18:04 +0000

    - create mindmap - create documentation folder

---
+++ global/documentation/gantry.mm
@@ -0,0 +1,144 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="gantry &quot;program&quot; organisation" FOLDED="false" ID="ID_684440859" CREATED="1489073854888" MODIFIED="1489074062984" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.683">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="modules" POSITION="right" ID="ID_555094537" CREATED="1489074386665" MODIFIED="1489074398153">
+<edge COLOR="#007c00"/>
+<node TEXT="control code" ID="ID_98402864" CREATED="1489073875064" MODIFIED="1489074401657">
+<node TEXT="includes movement + camera" ID="ID_1561674368" CREATED="1489073882200" MODIFIED="1489073890418"/>
+</node>
+<node TEXT="&apos;arena&apos; code" ID="ID_1180837855" CREATED="1489073891544" MODIFIED="1489074409149">
+<node TEXT="i.e. code for avoiding boxes" ID="ID_1892666215" CREATED="1489073897971" MODIFIED="1489073909698"/>
+</node>
+<node TEXT="image databases" ID="ID_1850912186" CREATED="1489073919169" MODIFIED="1489074409156">
+<node TEXT="2D" ID="ID_1965020702" CREATED="1489073923652" MODIFIED="1489073926202"/>
+<node TEXT="3D" ID="ID_938210353" CREATED="1489073926873" MODIFIED="1489073929731"/>
+<node TEXT="combine the two!" ID="ID_1969513250" CREATED="1489073931889" MODIFIED="1489073948346"/>
+<node TEXT="routes within databases?" ID="ID_877268886" CREATED="1489074960259" MODIFIED="1489074966363">
+<node TEXT="&quot;pure&quot;" ID="ID_734002030" CREATED="1489074966364" MODIFIED="1489074971691"/>
+<node TEXT="&quot;hybrid&quot;" ID="ID_617002533" CREATED="1489074972075" MODIFIED="1489074976331">
+<node TEXT="(i.e. using separate set of snaps)" ID="ID_635726981" CREATED="1489074976332" MODIFIED="1489074998124"/>
+</node>
+</node>
+</node>
+<node TEXT="&apos;live&apos; tests" ID="ID_844596824" CREATED="1489073956089" MODIFIED="1489074409162">
+<node TEXT="route following" ID="ID_639881575" CREATED="1489073966331" MODIFIED="1489073973445"/>
+<node TEXT="how to make pluggable?" ID="ID_1783893151" CREATED="1489074472802" MODIFIED="1489074483579"/>
+</node>
+<node TEXT="&quot;package&quot; for student" ID="ID_725933893" CREATED="1489073974625" MODIFIED="1489074409168"/>
+<node TEXT="data" ID="ID_328307177" CREATED="1489074001602" MODIFIED="1489074409174">
+<node TEXT="specific" ID="ID_347755339" CREATED="1489074857377" MODIFIED="1489074861555">
+<node TEXT="imdbs" ID="ID_1612855590" CREATED="1489074822744" MODIFIED="1489074831873"/>
+<node TEXT="live" ID="ID_394987882" CREATED="1489074832336" MODIFIED="1489074857058"/>
+</node>
+<node TEXT="general" ID="ID_569413117" CREATED="1489074874769" MODIFIED="1489074876522">
+<node TEXT="save to gantry_data in current folder" ID="ID_1707926595" CREATED="1489074890425" MODIFIED="1489075084493"/>
+<node TEXT="separate important (e.g. imdb) from unimportant (e.g. figdat)?" ID="ID_728944602" CREATED="1489075097501" MODIFIED="1489075131191"/>
+</node>
+</node>
+<node TEXT="my work" ID="ID_1396558418" CREATED="1489074711798" MODIFIED="1489074714791">
+<node TEXT="clearly separate!" ID="ID_1059794381" CREATED="1489074714792" MODIFIED="1489074725791">
+<node TEXT="g_alex?" ID="ID_1582868284" CREATED="1489074920683" MODIFIED="1489074922963"/>
+</node>
+</node>
+<node TEXT="documentation" ID="ID_1696534843" CREATED="1489074011842" MODIFIED="1489074409181">
+<node TEXT="&quot;how-tos&quot;" ID="ID_202654778" CREATED="1489074015650" MODIFIED="1489074031259"/>
+<node TEXT="code documentation" ID="ID_1756137419" CREATED="1489074031626" MODIFIED="1489074037067"/>
+<node TEXT="also commenting..." ID="ID_948511177" CREATED="1489074037667" MODIFIED="1489074046828"/>
+</node>
+</node>
+<node TEXT="naming" POSITION="left" ID="ID_1542614218" CREATED="1489074743455" MODIFIED="1489074745943">
+<edge COLOR="#007c7c"/>
+<node TEXT="start filenames with g_, then module name" ID="ID_1441438114" CREATED="1489074745944" MODIFIED="1489074793568"/>
+<node TEXT="e.g. g_arena_*.m" ID="ID_1674989397" CREATED="1489074752679" MODIFIED="1489074776384"/>
+<node TEXT="snakecase" ID="ID_229970803" CREATED="1489075154710" MODIFIED="1489075161287"/>
+</node>
+<node TEXT="sensible defaults" POSITION="left" ID="ID_1135341731" CREATED="1489074928898" MODIFIED="1489074934691">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="delete unneeded functions" POSITION="right" ID="ID_205550712" CREATED="1489075330089" MODIFIED="1489075335619">
+<edge COLOR="#ff0000"/>
+</node>
+</node>
+</map>
