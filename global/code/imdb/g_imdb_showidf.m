548fbb6143150215dad35d495964bba61bdfa5e8
Alex Dewar <alex.dewar@gmx.co.uk>
2017-03-22 11:16:52 +0000

    fix up IDF code to work with 3D imdbs and rename

--- global/code/imdb/g_imdb_showidf.m
+++ global/code/imdb/g_imdb_showidf.m
@@ -1,3 +1,7 @@
+function g_imdb_showidf(whz)
+if ~nargin
+    whz = 1;
+end
 
 whd = g_imdb_choosedb;
 load(fullfile(whd,'im_params.mat'));
@@ -5,20 +9,16 @@ load(fullfile(whd,'im_params.mat'));
 refxi = round(1+(length(p.xs)-1)/2);
 refyi = round(1+(length(p.ys)-1)/2);
 
-load('gantry_cropparams.mat')
-
 preprocess = @deal;
 
-load(fullfile(whd,sprintf('im_%03d_%03d.mat',refyi,refxi)),'fr');
-reffr = preprocess(im2gray(fr(y1:y2,:)));
+reffr = im2double(preprocess(g_imdb_getim(whd,refxi,refyi,whz)));
 
-[idf,hc] = deal(NaN(length(p.ys),length(p.xs)));
+idf = NaN(length(p.ys),length(p.xs));
 for yi = 1:size(idf,1)
     for xi = 1:size(idf,2)
-        fname = fullfile(whd,sprintf('im_%03d_%03d.mat',yi,xi));
-        if exist(fname,'file')
-            load(fname,'fr');
-            idf(yi,xi) = 255\getRMSdiff(preprocess(im2gray(fr(y1:y2,:))),reffr);
+        fr = g_imdb_getim(whd,xi,yi,whz);
+        if ~isempty(fr)
+            idf(yi,xi) = getRMSdiff(im2double(preprocess(fr)),reffr);
         end
     end
 end
