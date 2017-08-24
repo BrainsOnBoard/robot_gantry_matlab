ab28a708943d590011b5c0a95fbaac30a80bf341
Alex Dewar <a.dewar@sussex.ac.uk>
2017-08-24 12:11:21 +0100

    look for png not mat files in g_imdb_getimpts

--- global/code/imdb/g_imdb_getimpts.m
+++ global/code/imdb/g_imdb_getimpts.m
@@ -9,7 +9,7 @@ fex = false(length(p.xs),length(p.ys),length(p.zs));
 for xi = 1:length(p.xs)
     for yi = 1:length(p.ys)
         for zi = 1:length(p.zs)
-            fex(xi,yi,zi) = exist(fullfile(whd,sprintf('im_%03d_%03d_%03d.mat',xi,yi,zi)),'file');
+            fex(xi,yi,zi) = exist(fullfile(whd,sprintf('im_%03d_%03d_%03d.png',xi,yi,zi)),'file');
         end
     end
 end
