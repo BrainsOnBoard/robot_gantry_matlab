12595e8851adef04ecff01e88f21245178672c4e
Alex Dewar <a.dewar@sussex.ac.uk>
2017-08-24 12:22:45 +0100

    allow for just examining one z height with g_imdb_getimpts

--- global/code/imdb/g_imdb_getimpts.m
+++ global/code/imdb/g_imdb_getimpts.m
@@ -1,15 +1,21 @@
-function [fex,p,whd] = g_imdb_getimpts(whd)
-if ~nargin
+function [fex,p,whd] = g_imdb_getimpts(whd,p,zis)
+if nargin < 1 || isempty(whd)
     whd = g_imdb_choosedb;
 end
+if nargin < 2 || isempty(p)
+    load(fullfile(whd,'im_params.mat'))
+end
+if nargin < 3
+    zis = 1:length(p.zs);
+else
+    zis = zis(:)';
+end
 
-load(fullfile(whd,'im_params.mat'))
-
-fex = false(length(p.xs),length(p.ys),length(p.zs));
+fex = false(length(p.xs),length(p.ys),length(zis));
 for xi = 1:length(p.xs)
     for yi = 1:length(p.ys)
-        for zi = 1:length(p.zs)
-            fex(xi,yi,zi) = exist(fullfile(whd,sprintf('im_%03d_%03d_%03d.png',xi,yi,zi)),'file');
+        for i = 1:length(zis)
+            fex(xi,yi,i) = exist(fullfile(whd,sprintf('im_%03d_%03d_%03d.png',xi,yi,zis(i))),'file');
         end
     end
 end
