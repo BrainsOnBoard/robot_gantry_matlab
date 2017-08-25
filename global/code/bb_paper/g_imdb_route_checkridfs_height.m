4bfb29c2c5ad785a7c2644912d9efa5a07b8e4d4
Alex Dewar <a.dewar@sussex.ac.uk>
2017-08-25 15:02:16 +0100

    show ridf for clicked-on position in quiver plots

--- global/code/bb_paper/g_imdb_route_checkridfs_height.m
+++ global/code/bb_paper/g_imdb_route_checkridfs_height.m
@@ -1,11 +1,5 @@
-
 clear
 close all
-
-% x = 2000;
-% y = 1400;
-x = 1600;
-y = 1500;
 
 nth = 360;
 improc = '';
@@ -19,21 +13,44 @@ routenum = 3;
 
 arenafn = 'arena2_pile';
 
-crop = load('gantry_cropparams.mat');
-bestsnap = NaN(length(zht),1);
-myridfs = NaN(nth,length(zht));
+%% load RIDFs
 for i = 1:length(zht)
     [imxi,imyi,heads,whsn,err,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn,ridfs] = g_imdb_route_getrealsnapserrs3d(shortwhd,arenafn,routenum,res,zht(i),false,improc,forcegen);
-    
-    xi = find(x==p.xs);
-    yi = find(y==p.ys);
-    sel = find(xi==imxi & yi==imyi);
-    bestsnap(i) = whsn(sel);
-    myridfs(:,i) = shiftdim(ridfs(sel,:,bestsnap(i)));
+
+    bestsnap(:,i) = whsn;
+    for j = 1:size(ridfs,1)
+        bestridfs(j,:,i) = ridfs(j,:,whsn(j));
+    end
 end
 
-%%
-figure(1);clf
-plot(myridfs)
-axis tight
-legend(num2str(zht'))
+%% show quiver plot
+g_imdb_route_showrealsnapsquiver3d(false,false,improc,shortwhd,zht);
+
+while true
+    figure(1)
+    [x,y,but] = ginput(1);
+    if isempty(but)
+        break
+    end
+    if but ~= 1
+        continue
+    end
+    if x < 0 || x > p.lim(1) || y < 0 || y > p.lim(2)
+        disp('Invalid point selected')
+        continue
+    end
+    
+    [~,xi] = min(abs(p.xs-x));
+    [~,yi] = min(abs(p.ys-y));
+    sel = xi==imxi & yi==imyi;
+    if ~any(sel)
+        disp('Invalid point selected')
+        continue
+    end
+    
+    figure(2);clf
+    plot(shiftdim(bestridfs(sel,:,:)))
+    xlim([0 nth])
+    title(sprintf('x=%d, y=%d',p.xs(xi),p.ys(yi)))
+    legend(num2str(zht'))
+end
