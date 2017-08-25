a6b44e413f40d335af3cc0d3a47a8f7995441268
Alex Dewar <a.dewar@sussex.ac.uk>
2017-08-25 16:57:20 +0100

    allow for saving ridfs for fixed coordinates

--- global/code/bb_paper/g_imdb_route_checkridfs_height.m
+++ global/code/bb_paper/g_imdb_route_checkridfs_height.m
@@ -1,7 +1,4 @@
-function g_imdb_route_checkridfs_height
-
-%%
-clear
+function g_imdb_route_checkridfs_height(dosave)
 close all
 
 nth = 360;
@@ -16,6 +13,9 @@ routenum = 3;
 
 arenafn = 'arena2_pile';
 
+% fixed coordinates to save RIDFs for if dosave==true
+coords = [1600 1500; 1600 1600; 1500 1600; 1900 1300; 2100 1300; 2300 1200; 2400 1100];
+
 %% load RIDFs
 for i = 1:length(zht)
     [imxi,imyi,heads,whsn,err,nearest,dist,snx,sny,snth,errsel,p,isnew,allwhsn,ridfs] = g_imdb_route_getrealsnapserrs3d(shortwhd,arenafn,routenum,imsz(2),zht(i),false,improc,forcegen);
@@ -27,50 +27,62 @@ for i = 1:length(zht)
 end
 bestridfs = bestridfs/prod(imsz);
 
-%% show quiver plot
-g_imdb_route_showrealsnapsquiver3d(false,false,improc,shortwhd,zht);
-
+%%
 imfun = gantry_getimfun(improc);
 [snaps,~,snx,sny,snth]=g_imdb_route_getrealsnaps3d(arenafn,routenum,imsz(2),improc);
-while true
-    figure(1)
-    try
-        [x,y,but] = ginput(1);
-    catch ex
-        if strcmp(ex.identifier,'MATLAB:ginput:FigureDeletionPause')
+
+if dosave
+    for i = 1:size(coords,1)
+        xi = find(p.xs==coords(i,1));
+        yi = find(p.ys==coords(i,2));
+        sel = xi==imxi & yi==imyi;
+        
+        showridfs(coords(i,1),coords(i,2),xi,yi,bestridfs(sel,:,:),bestsnap(sel,:),zht,snx,sny,snaps,imfun,whd,imsz(2),p)
+        saverealsnapsridfs(coords(i,1),coords(i,2))
+    end
+else
+    g_imdb_route_showrealsnapsquiver3d(false,false,improc,shortwhd,zht);
+    while true
+        figure(1)
+        try
+            [x,y,but] = ginput(1);
+        catch ex
+            if strcmp(ex.identifier,'MATLAB:ginput:FigureDeletionPause')
+                break
+            end
+            rethrow(ex)
+        end
+        if isempty(but)
             break
         end
-        rethrow(ex)
-    end
-    if isempty(but)
-        break
-    end
-    
-    if but==1
-        if x < 0 || x > p.lim(1) || y < 0 || y > p.lim(2)
-            disp('Invalid point selected')
-            continue
+        
+        if but==1
+            if x < 0 || x > p.lim(1) || y < 0 || y > p.lim(2)
+                disp('Invalid point selected')
+                continue
+            end
+            
+            [~,xi] = min(abs(p.xs-x));
+            [~,yi] = min(abs(p.ys-y));
+            sel = xi==imxi & yi==imyi;
+            if ~any(sel)
+                disp('Invalid point selected')
+                continue
+            end
+            
+            gx = p.xs(xi);
+            gy = p.ys(yi);
+            
+            showridfs(gx,gy,xi,yi,bestridfs(sel,:,:),bestsnap(sel,:),zht,snx,sny,snaps,imfun,whd,imsz(2),p)
+        elseif but=='s'
+            saverealsnapsridfs(gx,gy);
         end
-        
-        [~,xi] = min(abs(p.xs-x));
-        [~,yi] = min(abs(p.ys-y));
-        sel = xi==imxi & yi==imyi;
-        if ~any(sel)
-            disp('Invalid point selected')
-            continue
-        end
-        
-        gx = p.xs(xi);
-        gy = p.ys(yi);
-        fprintf('selecting point (%d,%d)\n',gx,gy)
-        
-        showridfs(gx,gy,xi,yi,bestridfs(sel,:,:),bestsnap(sel,:),zht,snx,sny,snaps,imfun,whd,imsz(2),p)
-    elseif but=='s'
-        saveridfs(gx,gy);
     end
 end
 
 function showridfs(gx,gy,xi,yi,bestridfs,bestsnap,zht,snx,sny,snaps,imfun,whd,res,p)
+fprintf('selecting point (%d,%d)\n',gx,gy)
+
 figure(2);clf
 alsubplot(3+length(zht),2,1:2,1:2)
 plot(shiftdim(bestridfs(1,:,:)))
@@ -98,6 +110,6 @@ for i = 1:length(zht)
     ylabel(whsn)
 end
 
-function saveridfs(gx,gy)
+function saverealsnapsridfs(gx,gy)
 figure(2)
-g_fig_save(sprintf('ridf_%04d_%04d',gx,gy),[30 15]);
+g_fig_save(sprintf('ridf_%04d_%04d',gx,gy),[30 30]);
