8e09a76d631124083f0eb85dff3724173a41c3c0
Alex Dewar <a.dewar@sussex.ac.uk>
2017-10-27 15:13:34 +0100

    tweaks and fixes for g_imdb_route_showrealsnapsquiver3d

--- global/code/bb_paper/g_imdb_route_showrealsnapsquiver3d.m
+++ global/code/bb_paper/g_imdb_route_showrealsnapsquiver3d.m
@@ -1,17 +1,18 @@
-function g_imdb_route_showrealsnapsquiver3d(dosave,douseinfomax,improc,shortwhd,zht)
+function g_imdb_route_showrealsnapsquiver3d(dosave,useinfomax,improc,shortwhd,zht)
 if nargin < 1
     dosave = false;
 end
 if nargin < 2
-    douseinfomax = [false true];
+    useinfomax = [false true];
 end
 if nargin < 3
     improc = '';
 end
 if nargin < 4
     shortwhd={
-    'imdb_2017-02-09_001'      % open, new boxes
-    %'imdb_2016-03-23_001', ... % open, empty
+        'imdb_2017-02-09_001' % open, pile
+%         'imdb_2016-03-23_001' % open, empty
+%         'imdb_2016-03-29_001' % open, boxes
     };
 elseif ~iscell(shortwhd)
     shortwhd = {shortwhd};
@@ -25,6 +26,10 @@ forcegen = false;
 
 res = 90;
 routenums = 3;
+% routenums = 1;
+
+arenafn = 'arena2_pile';
+% arenafn = 'arena1_boxes';
 
 if ~dosave
     figure(1);clf
@@ -33,24 +38,27 @@ if ~dosave
     subplot(2,spcols,1)
 end
 
-for useinfomax = douseinfomax
+for i = 1:length(useinfomax)
+    if ~dosave
+        figure(i);clf
+    end
     for cres = res
-        for i = 1:length(shortwhd)
+        for j = 1:length(shortwhd)
             for routenum = routenums
-                for j = 1:length(zht)
-                    [imxi,imyi,heads,whsn,err,nearest,dist,snx,sny,snth,errsel,p,isnew] = g_imdb_route_getrealsnapserrs3d(shortwhd{i},'arena2_pile',routenum,cres,zht(j),useinfomax,improc,forcegen);
+                for k = 1:length(zht)
+                    [imxi,imyi,heads,whsn,err,nearest,dist,snx,sny,snth,errsel,p,isnew] = g_imdb_route_getrealsnapserrs3d(shortwhd{j},arenafn,routenum,cres,zht(k),useinfomax(i),improc,forcegen);
                     
                     if newonly && ~isnew
                         continue
                     end
                     
-                    whd = fullfile(g_dir_imdb,shortwhd{i});
+                    whd = fullfile(g_dir_imdb,shortwhd{j});
                     flabel = g_imdb_getlabel(whd);
                     
                     if dosave
                         figure(1);clf
                     else
-                        subplot(2,spcols,j)
+                        subplot(2,spcols,k)
                     end
                     hold on
                     
@@ -67,12 +75,12 @@ for useinfomax = douseinfomax
                     axis equal tight
                     xlabel('x (mm)')
                     ylabel('y (mm)')
-                    if useinfomax
+                    if useinfomax(i)
                         methodstr = 'infomax';
                     else
                         methodstr = 'ridf';
                     end
-                    title(sprintf('%s (route %d, res %d, ht %d, %s)', flabel, routenum, cres, zht(j), methodstr))
+                    title(sprintf('%s (route %d, res %d, ht %d, %s)', flabel, routenum, cres, zht(k), methodstr))
                     g_fig_setfont
                     
                     if dosave
@@ -81,12 +89,12 @@ for useinfomax = douseinfomax
                         else
                             improcstr = [improc,'_'];
                         end
-                        if useinfomax
+                        if useinfomax(i)
                             algorithmstr = 'infomax';
                         else
                             algorithmstr = 'pm';
                         end
-                        g_fig_save(sprintf('%s%s_%s_route%d_res%03d_z%d_ridf_quiver%s',improcstr,algorithmstr,flabel,routenum,cres,zht(j)),[10 10]);
+                        g_fig_save(sprintf('%s%s_%s_route%d_res%03d_z%d_ridf_quiver%s',improcstr,algorithmstr,flabel,routenum,cres,zht(k)),[10 10]);
                     end
                 end
             end
