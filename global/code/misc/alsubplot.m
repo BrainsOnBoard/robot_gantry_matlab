a8a9db7d309590805de56f7c5f268280baf996d3
Alex Dewar <a.dewar@sussex.ac.uk>
2017-11-06 13:19:52 +0000

    add in alsubplot.m

---
+++ global/code/misc/alsubplot.m
@@ -0,0 +1,29 @@
+function alsubplot(a,b,row,col)
+persistent spsz
+
+if nargin==3
+    col = 1;
+end
+
+if nargin>2
+    spsz = [a b];
+elseif isempty(spsz)
+    error('subplot size must be specified');
+else
+    row = a;
+    if nargin==1
+        col = 1;
+    else
+        col = b;
+    end
+    a = spsz(1);
+    b = spsz(2);
+end
+
+if numel(row)==1 && numel(col)>1
+    row = row*ones(size(col));
+elseif numel(col)==1 && numel(row)>1
+    col = col*ones(size(row));
+end
+
+subplot(a,b,sub2ind([b a],col,row));
