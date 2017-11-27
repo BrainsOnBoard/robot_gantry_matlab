2145524fc202e7cc6a04f256fbc0bb17969c4adf
Alex Dewar <a.dewar@sussex.ac.uk>
2017-11-27 17:48:44 +0000

    fix up boxplots code

--- global/code/bb_paper/g_bb_boxplot.m
+++ global/code/bb_paper/g_bb_boxplot.m
@@ -1,27 +1,43 @@
-function g_bb_boxplot(dosave,shortwhd,routenums,improc)
-if nargin < 4 || isempty(improc)
-    improc = '';
-end
-if nargin < 3 || isempty(routenums)
-    routenums = 1;
-end
-if nargin < 2 || isempty(shortwhd)
-    shortwhd = {
-        'imdb_2017-06-06_001' % closed, plants
+function g_bb_boxplot(shortwhd,routenums,zht,snapszht,userealsnaps,useinfomax,improc,dosave,doseparateplots)
+close all
+
+if nargin < 1 || isempty(shortwhd)
+    shortwhd={
+        'imdb_2017-02-09_001'  % open, pile
+%         'imdb_2016-03-29_001' % open, boxes
+%         'imdb_2016-02-08_003' % closed, boxes (z=250mm)
+%         'imdb_2017-06-06_001' % closed, plants
         };
 elseif ~iscell(shortwhd)
     shortwhd = {shortwhd};
 end
-if nargin < 1 || isempty(dosave)
+if nargin < 2 || isempty(routenums)
+    routenums = 1;
+end
+if nargin < 3 || isempty(zht)
+    zht = 0:100:500;
+end
+if nargin < 4 || isempty(snapszht)
+    snapszht = 0:100:500;
+end
+if nargin < 5 || isempty(userealsnaps)
+    userealsnaps = false;
+end
+if nargin < 6 || isempty(useinfomax)
+    useinfomax = false;
+end
+if nargin < 7 || isempty(improc)
+    improc = '';
+end
+if nargin < 8 || isempty(dosave)
     dosave = false;
 end
+if nargin < 9 || isempty(doseparateplots)
+    doseparateplots = true;
+end
 
-useinfomax = [false true];
-userealsnaps = false;
-snapszht = 200;
 improcforinfomax = false;
 res = 90;
-zht = 0:100:500;
 forcegen = false;
 
 errs = cell(length(useinfomax),length(res),length(shortwhd), ...
@@ -37,7 +53,7 @@ for i = 1:length(useinfomax)
                             shortwhd{k},routenums(m),res(j),zht(n), ...
                             useinfomax(i),improc,forcegen,improcforinfomax, ...
                             userealsnaps,snapszht(l));
-
+                        
                         errs{i,j,k,l,m,n} = err(errsel);
                     end
                 end
@@ -51,9 +67,11 @@ if dosave
         improc(end+1) = '_';
     end
     flabel = g_imdb_getlabel(fullfile(g_dir_imdb,shortwhd{1}));
-    
-    g_fig_series_start
 end
+
+% if dosave && doseparateplots
+%     g_fig_series_start
+% end
 for m = 1:length(routenums)
     for l = 1:length(snapszht)
         if dosave
@@ -61,26 +79,36 @@ for m = 1:length(routenums)
         else
             figure((l-1)*length(routenums) + m);clf
         end
-        if length(useinfomax)==2
+        if ~doseparateplots
             subplot(2,1,1)
         end
         doboxplot(errs(1,:,:,l,m,:),zht);
-        title(sprintf('Perfect memory (route %d, snapht %d)',routenums(m),snapszht(l)))
+        title(sprintf('Perfect memory (snapshot height: %d mm)',snapszht(l)+50));
+        g_fig_setfont
         
-        if length(useinfomax)==2
-            subplot(2,1,2)
-            doboxplot(errs(2,:,:,l,m,:),zht);
-            title('Infomax')
+        if dosave && doseparateplots
+            g_fig_save(sprintf('boxplot_%s_%s%sres%03d_route%03d_snapszht%03d',flabel,improc,'pm_',res,routenums(m),snapszht(l)),[20 10]);
         end
         
+        if doseparateplots
+            figure(2);clf
+            algstr = 'infomax_';
+        else
+            subplot(2,1,2)
+            algstr = '';
+        end
+        doboxplot(errs(2,:,:,l,m,:),zht);
+        title(sprintf('Infomax (snapshot height: %d mm)',snapszht(l)+50))
+        g_fig_setfont
+        
         if dosave
-            g_fig_save(sprintf('boxplot_%s_%sres%03d_route%03d_snapszht%03d',flabel,improc,res,routenums(m),snapszht(l)),[20 10]);
+            g_fig_save(sprintf('boxplot_%s_%s%sres%03d_route%03d_snapszht%03d',flabel,improc,algstr,res,routenums(m),snapszht(l)),[20 10]);
         end
     end
 end
-if dosave
-    g_fig_series_end(sprintf('boxplot_%s_%sres%03d.pdf',flabel,improc,res))
-end
+% if dosave && doseparateplots
+%     g_fig_series_end(sprintf('boxplot_%s_%sres%03d.pdf',flabel,improc,res))
+% end
 
 function doboxplot(errs,zht)
 hold on
@@ -95,4 +123,4 @@ set(gca,'XTick',1:i,'XTickLabel',zht+50)
 
 xlabel('Test height (mm)')
 ylabel('Error (deg)')
-g_fig_setfont
+
--- global/code/bb_paper/g_bb_make.m
+++ global/code/bb_paper/g_bb_make.m
@@ -1,9 +1,18 @@
+%%
 dosave = true;
 useinfomax = [false true];
+improc = '';
 
 %% quivers
 % open, pile
-g_bb_showdata('imdb_2017-02-09_001',1,[],200,true,useinfomax,'',true,false,true,false);
+g_bb_showdata('imdb_2017-02-09_001',1,[],200,true,useinfomax,improc,true,false,true,false);
 
 % open, boxes
-g_bb_showdata('imdb_2016-03-29_001',3,[],200,false,useinfomax,'',true,false,true,false);
+g_bb_showdata('imdb_2016-03-29_001',3,[],200,false,useinfomax,improc,true,false,true,false);
+
+%% boxplots
+% open, pile
+g_bb_boxplot('imdb_2017-02-09_001',1,[],200,true,useinfomax,improc,true,true);
+
+% open, boxes
+g_bb_boxplot('imdb_2016-03-29_001',3,[],200,false,useinfomax,improc,true,true);
