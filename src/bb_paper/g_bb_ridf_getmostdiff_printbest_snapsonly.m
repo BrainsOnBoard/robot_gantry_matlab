15127412bce52ca11c7791a5b1fd37a87b69fc18
Alex Dewar <a.dewar@sussex.ac.uk>
2018-01-12 11:22:17 +0000

    code for bob meeting pres figures

---
+++ src/bb_paper/g_bb_ridf_getmostdiff_printbest_snapsonly.m
@@ -0,0 +1,37 @@
+function g_bb_ridf_getmostdiff_printbest_snapsonly
+[~,shortwhd] = g_imdb_choosedb;
+
+routenum = input('Enter route number [1]: ');
+if isempty(routenum)
+    routenum = 1;
+end
+
+improc = '';
+snapszht = 200;
+snapszhtall = 0:100:500;
+[minima,imxi,imyi,p] = g_bb_ridf_getmostdiff_snapsonly(shortwhd,routenum,[],snapszhtall,false,improc);
+
+%% get biggest diffs for given snapszht
+ind = snapszhtall==snapszht;
+[~,I] = sort(minima(:,end,ind)-minima(:,1,ind),'descend');
+
+fprintf('\n%% biggest diffs for snapszht=%d\n',snapszht)
+printcoords(I);
+
+%% get biggest average diffs across snapszhts
+[~,I] = sort(mean(minima(:,end,:)-minima(:,1,:),3));
+
+disp('% biggest mean diffs across snapzhts')
+printcoords(I);
+
+    function printcoords(imind)
+        coords = [p.xs(imxi(imind))' p.ys(imyi(imind))'];
+
+        fprintf('coords = [%d %d',coords(1,:))
+        for i = 2:10
+            fprintf('; %d %d',coords(i,:))
+        end
+        fprintf('];\n\n');
+    end
+
+end
---
+++ src/bb_paper/g_bobmeeting_make.m
@@ -0,0 +1,45 @@
+%%
+clear
+
+dosave = true;
+useinfomax = false; %[false true];
+improc = '';
+% realsnapsel = 3;
+realsnapsel = false;
+snapszhtall = [0 100 400 500];
+zht = snapszhtall;
+
+arena_pile = 'imdb_2017-02-09_001';
+arena_boxes = 'imdb_2016-03-29_001';
+arena_plants = 'imdb_2017-06-06_001';
+
+%% quivers
+% open, pile
+g_bb_quiver(arena_pile,1,zht,snapszhtall,realsnapsel,useinfomax,improc,true,false,dosave,false);
+
+% % open, boxes
+% g_bb_quiver(arena_boxes,3,zht,snapszhtall,false,useinfomax,improc,true,false,dosave,false);
+
+% plants
+g_bb_quiver(arena_plants,1,zht,snapszhtall,realsnapsel,useinfomax,improc,true,false,dosave,false);
+
+%% boxplots
+% open, pile
+g_bb_boxplot(arena_pile,1,zht,snapszhtall,false,useinfomax,improc,dosave,false);
+
+% % open, boxes
+% g_bb_boxplot(arena_boxes,3,zht,snapszhtall,false,useinfomax,improc,dosave,false);
+
+% plants
+g_bb_boxplot(arena_plants,1,zht,snapszhtall,false,useinfomax,improc,dosave,false);
+
+%% RIDFs
+% % biggest diffs for snapszht=200
+% coords = [400 1300; 200 1400; 200 1500; 300 1300; 300 1400; 1200 900; 1400 900; 1300 900; 100 1500; 1000 900];
+
+% % biggest mean diffs across snapzhts
+% coords = [800 900; 700 1000; 700 900; 600 1000; 900 900; 500 1100; 600 1100; 500 1200; 1600 800; 400 1200];
+coords = [800 900];
+
+% open, pile
+g_bb_ridf(arena_plants,1,zht,snapszhtall,false,improc,coords,dosave,true);
