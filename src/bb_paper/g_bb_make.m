c97e966a384f8c253d12dd6b45e3977fce11033c
Unknown <a.dewar@sussex.ac.uk>
2018-01-22 18:02:25 +0000

    extra quivers

--- src/bb_paper/g_bb_make.m
+++ src/bb_paper/g_bb_make.m
@@ -6,8 +6,9 @@ figtype = 'svg';
 useinfomax = [false true];
 improc = '';
 % realsnapsel = 3;
-snapszhtreal = 200;
-snapszhtall = 0:100:500;
+snapszht1 = 200;
+snapszht2 = 0;
+zht = 0:100:500;
 
 doquiver = true;
 dowhsn = false;
@@ -16,25 +17,35 @@ arena_pile = 'imdb_2017-02-09_001';
 arena_boxes = 'imdb_2016-03-29_001';
 arena_plants = 'imdb_2017-06-06_001';
 
-%% quivers
+%% quivers 1
 % open, pile
-g_bb_quiver(arena_pile,1,[],snapszhtreal,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
+g_bb_quiver(arena_pile,1,[],snapszht1,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
 
-% open, boxes
-g_bb_quiver(arena_boxes,3,[],snapszhtreal,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
+% % open, boxes
+% g_bb_quiver(arena_boxes,3,[],snapszht1,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
 
 % plants
-g_bb_quiver(arena_plants,1,[],snapszhtreal,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
+g_bb_quiver(arena_plants,1,[],snapszht1,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
+
+%% quivers 2
+% open, pile
+g_bb_quiver(arena_pile,1,[],snapszht2,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
+
+% % open, boxes
+% g_bb_quiver(arena_boxes,3,[],snapszht2,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
+
+% plants
+g_bb_quiver(arena_plants,1,[],snapszht2,false,useinfomax,improc,doquiver,dowhsn,false,dosave,figtype);
 
 %% boxplots
 % open, pile
-g_bb_boxplot(arena_pile,1,[],snapszhtall,false,useinfomax,improc,false,dosave,figtype);
+g_bb_boxplot(arena_pile,1,[],zht,false,useinfomax,improc,false,dosave,figtype);
 
-% open, boxes
-g_bb_boxplot(arena_boxes,3,[],snapszhtall,false,useinfomax,improc,false,dosave,figtype);
+% % open, boxes
+% g_bb_boxplot(arena_boxes,3,[],zht,false,useinfomax,improc,false,dosave,figtype);
 
 % plants
-g_bb_boxplot(arena_plants,1,[],snapszhtall,false,useinfomax,improc,false,dosave,figtype);
+g_bb_boxplot(arena_plants,1,[],zht,false,useinfomax,improc,false,dosave,figtype);
 
 %% RIDFs
 % % old coords
