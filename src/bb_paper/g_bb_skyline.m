df028e91913f92133701b5693486e4552113ef5e
Alex Dewar <a.dewar@sussex.ac.uk>
2018-01-29 16:34:18 +0000

    normalise ims; do one z at a time

--- src/bb_paper/g_bb_skyline.m
+++ src/bb_paper/g_bb_skyline.m
@@ -1,3 +1,21 @@
-clear
+function g_bb_skyline
 
-andy_skyline('green_imdb_2017-06-06_001',[],[],true);
+shortwhd = 'green_imdb_2017-06-06_001';
+load(fullfile(g_dir_imdb,shortwhd,'im_params.mat'),'p');
+tic
+for i = 1:length(p.zs)
+    if andy_skyline(shortwhd,sprintf('*_%03d.png',i),@stretch,false)
+        break
+    end
+end
+toc
+
+function im=stretch(im)
+% im = im2double(im);
+% mins = min(im);
+% range = max(im)-mins;
+% im = bsxfun(@rdivide,bsxfun(@minus,im,mins),range);
+im = double(im);
+minval = min(im(:));
+range = max(im(:))-minval;
+im = im2uint8((im - minval) / range);
