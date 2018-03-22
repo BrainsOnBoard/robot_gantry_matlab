e2d338a3a5d1a78abc190a1bb7d0096b71f75e91
Unknown <a.dewar@sussex.ac.uk>
2018-03-22 13:51:02 +0000

    do best and worst examples

--- src/bb_paper/g_bb_bestworst_examples.m
+++ src/bb_paper/g_bb_bestworst_examples.m
@@ -26,12 +26,17 @@ shortwhd = {
     'imdb_2017-06-06_001'  % plants
     };
 coords = {[
-    2700 1000; % d14. selecting snaps further from pile with height
-    600 500    % d16. good perf with distant snap
+    2700 1000; % gd14. selecting snaps further from pile with height
+    600  500   % gd16. good perf with distant snap
+    1400 0;    % b1. selecting snap from opposite side
+    0    400   % b13. @550, at end of route, selecting snap from start
     ], [
-    600 800; % d1. good example
-    300 1500 % d3. low err with far-away snap
+    600  800; % gd1. good example
+    300  1500 % gd3. low err with far-away snap
     ]};
+getbest = {
+    [true(2,1);false(2,1)];
+    true(2,1)};
 routenum = [1 1];
 
 for i = 1:length(shortwhd)
@@ -43,15 +48,25 @@ for i = 1:length(shortwhd)
     dat.allerrs = dat.allerrs';
     dat.whsn = dat.whsn';
 
-    getbest = true;
-
-    if getbest
+    bests = getbest{i};
+    if any(bests)
+        disp('doing best-matching')
+        
         fprefix = 'best_pub_';
-    else
-        fprefix = 'worst_pub_';
+        g_bb_ridf(shortwhd{i},routenum(1),zht,snapszht,userealsnaps, ...
+            improc,coords{i}(bests,:),shiftridfs,dosave,joinpdfs,figtype, ...
+            doautoridf,dointeractive,dat.headings,dat.errs,dat.allerrs, ...
+            fprefix,pubgrade,ridfx360,skipexisting)
     end
-    g_bb_ridf(shortwhd{i},routenum(1),zht,snapszht,userealsnaps,improc, ...
-        coords{i},shiftridfs,dosave,joinpdfs,figtype, ...
-        doautoridf,dointeractive,dat.headings,dat.errs,dat.allerrs,fprefix, ...
-        pubgrade,ridfx360,skipexisting)
+    
+    worsts = ~bests;
+    if any(worsts)
+        disp('doing worst-matching')
+        
+        fprefix = 'worst_pub_';
+        g_bb_ridf(shortwhd{i},routenum(1),zht,snapszht,userealsnaps, ...
+            improc,coords{i}(worsts,:),shiftridfs,dosave,joinpdfs,figtype, ...
+            doautoridf,dointeractive,dat.headings,dat.errs,dat.allerrs, ...
+            fprefix,pubgrade,ridfx360,skipexisting)
+    end
 end
