a6046d6ff4311f46b20f8cf1e023245ad9944002
Unknown <a.dewar@sussex.ac.uk>
2018-03-23 11:45:45 +0000

    fix: select correct values for "worst" ridfs

--- src/bb_paper/g_bb_bestworst_examples.m
+++ src/bb_paper/g_bb_bestworst_examples.m
@@ -27,7 +27,7 @@ shortwhd = {
     };
 coords = {[
     2700 1000; % gd14. selecting snaps further from pile with height
-    600  500   % gd16. good perf with distant snap
+    600  500;  % gd16. good perf with distant snap
     1400 0;    % b1. selecting snap from opposite side
     0    400   % b13. @550, at end of route, selecting snap from start
     ], [
@@ -35,7 +35,7 @@ coords = {[
     300  1500 % gd3. low err with far-away snap
     ]};
 getbest = {
-    [true(2,1);false(2,1)];
+    [true(2,1); false(2,1)];
     true(2,1)};
 routenum = [1 1];
 
@@ -43,7 +43,6 @@ for i = 1:length(shortwhd)
     [dat.headings,dat.allerrs,dat.whsn] = g_bb_getdatapartial( ...
         coords{i},shortwhd{i},routenum(1),res,zht,false,improc,false, ...
         [],false,snapszht,dosave);
-    dat.errs = mean(dat.allerrs);
     dat.headings = dat.headings';
     dat.allerrs = dat.allerrs';
     dat.whsn = dat.whsn';
@@ -52,10 +51,13 @@ for i = 1:length(shortwhd)
     if any(bests)
         disp('doing best-matching')
         
+        bheads = dat.headings(bests,:);
+        ballerrs = dat.allerrs(bests,:);
+        berrs = mean(ballerrs);
         fprefix = 'best_pub_';
         g_bb_ridf(shortwhd{i},routenum(1),zht,snapszht,userealsnaps, ...
             improc,coords{i}(bests,:),shiftridfs,dosave,joinpdfs,figtype, ...
-            doautoridf,dointeractive,dat.headings,dat.errs,dat.allerrs, ...
+            doautoridf,dointeractive,bheads,berrs,ballerrs, ...
             fprefix,pubgrade,ridfx360,skipexisting)
     end
     
@@ -63,10 +65,13 @@ for i = 1:length(shortwhd)
     if any(worsts)
         disp('doing worst-matching')
         
+        wheads = dat.headings(worsts,:);
+        wallerrs = dat.allerrs(worsts,:);
+        werrs = mean(wallerrs);
         fprefix = 'worst_pub_';
         g_bb_ridf(shortwhd{i},routenum(1),zht,snapszht,userealsnaps, ...
             improc,coords{i}(worsts,:),shiftridfs,dosave,joinpdfs,figtype, ...
-            doautoridf,dointeractive,dat.headings,dat.errs,dat.allerrs, ...
+            doautoridf,dointeractive,wheads,werrs,wallerrs, ...
             fprefix,pubgrade,ridfx360,skipexisting)
     end
 end
