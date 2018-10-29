6d2eba0f45d603abab6222811c8cb62920587474
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-29 17:30:22 +0000

    Restore script for converting .mat to .png

--- src/imdb/convert/g_imdb_mat2png.m
+++ src/imdb/convert/g_imdb_mat2png.m
@@ -1,14 +1,11 @@
 function g_imdb_mat2png
 
-oldpwd = pwd;
-
-cd(g_dir_imdb);
 d = dir;
 for i = 1:length(d)
     if d(i).isdir && d(i).name(1)~='.'
         fprintf('entering %s\n',d(i).name);
         cd(d(i).name);
-        imfns = dir(fullfile('im_*_*_*.mat'));
+        imfns = dir(fullfile('im_*_*.mat'));
         for j = 1:length(imfns)
             load(imfns(j).name,'fr');
             imwrite(fr,[imfns(j).name(1:end-4) '.png']);
@@ -17,5 +14,3 @@ for i = 1:length(d)
         cd('..');
     end
 end
-
-cd(oldpwd);
