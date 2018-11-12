20ea6f11fe242365613f92d3154acbc7f7d35174
Alex Dewar <a.dewar@sussex.ac.uk>
2018-11-12 11:16:46 +0000

    Tweaks to convert script

--- src/imdb/convert/g_imdb_convert_to_bob_robotics_format.m
+++ src/imdb/convert/g_imdb_convert_to_bob_robotics_format.m
@@ -3,23 +3,39 @@ function g_imdb_convert_to_bob_robotics_format
 olddir = pwd;
 
 cd(g_dir_imdb_raw);
-d = dir('wrapped_*');
+d = dir;
 
 for i = 1:length(d)
+    if ~d(i).isdir
+        continue
+    end
+    
     dname = d(i).name;
+    if dname(1) == '.'
+        continue
+    end
     fprintf('Processing %s...\n', dname)
     
     cd(dname)
     load('im_params.mat','p');
     
+    % if it's a 2d database
+    is2d = ~isfield(p,'zs');
+    if is2d
+        p.zs = p.zht;
+    end
+    
     % get date from path
-    data.metadata.time = dname(length('wrapped_imdb_') + (1:10));
+    data.metadata.time = dname(1:10);
     assert(~varsinmatfile('im_params.mat','metadata'));
     
     % grid parameters
     data.metadata.type = 'grid';
     data.metadata.grid.beginAtMM = [p.xs(1) p.ys(1) p.zs(1)];
     data.metadata.grid.separationMM = p.imsep * ones(1,3);
+    if is2d
+        p.imsep(3) = 0;
+    end
     data.metadata.grid.size = [length(p.xs) length(p.ys) length(p.zs)];
     
     % camera properties
@@ -28,13 +44,27 @@ for i = 1:length(d)
     data.metadata.camera.isPanoramic = 1;
     
     % gantry-specific properties
-    data.metadata.gantry.innerLimits = p.lim(:)';
+    if isfield(p,'lim')
+        data.metadata.gantry.innerLimits = p.lim(:)';
+    end
     data.metadata.gantry.offset = p.offset(:)';
-    data.metadata.gantry.zClearance = p.zclear;
-    data.metadata.gantry.baseHeight = p.zoffs;
-    data.metadata.gantry.objectGridSpacing = p.objgridac;
-    data.metadata.gantry.headClearance = p.headclear;
-    data.metadata.gantry.arenaName = char(p.arenafn);
+    if isfield(p,'zclear')
+        data.metadata.gantry.zClearance = p.zclear;
+    end
+    if isfield(p,'zoffs')
+        data.metadata.gantry.baseHeight = p.zoffs;
+    end
+    if isfield(p,'objgridac')
+        data.metadata.gantry.objectGridSpacing = p.objgridac;
+    end
+    if isfield(p,'headclear')
+        data.metadata.gantry.headClearance = p.headclear;
+    end
+    if isfield(p,'arenafn')
+        data.metadata.gantry.arenaName = char(p.arenafn);
+    else
+        data.metadata.gantry.arenaName = '';
+    end
     if isfield(p,'maxA')
         data.metadata.gantry.maximumAcceleration = p.maxA(:)';
     end
