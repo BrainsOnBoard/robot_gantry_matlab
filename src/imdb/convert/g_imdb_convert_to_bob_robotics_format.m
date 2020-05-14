e4022f8943b015b8f8f84f71576b030856023b16
Alex Dewar <a.dewar@sussex.ac.uk>
2020-05-14 16:23:34 +0100

    Fix up script to convert image DBs to BoB style

--- src/imdb/convert/g_imdb_convert_to_bob_robotics_format.m
+++ src/imdb/convert/g_imdb_convert_to_bob_robotics_format.m
@@ -2,7 +2,7 @@ function g_imdb_convert_to_bob_robotics_format
 
 olddir = pwd;
 
-cd(g_dir_imdb_raw);
+cd(g_dir_imdb);
 d = dir;
 
 for i = 1:length(d)
@@ -17,83 +17,99 @@ for i = 1:length(d)
     fprintf('Processing %s...\n', dname)
     
     cd(dname)
-    load('im_params.mat','p');
+    params = load('im_params.mat');
     
     % if it's a 2d database
-    is2d = ~isfield(p,'zs');
+    is2d = ~isfield(params.p,'zs');
     if is2d
-        p.zs = p.zht;
+        params.p.zs = params.p.zht;
     end
     
     % get date from path
-    data.metadata.time = dname(1:10);
+    data.metadata.time = dname(end-13:end-4); % this will only be the date
+    datetime(data.metadata.time); % check it's actually a valid date
     assert(~varsinmatfile('im_params.mat','metadata'));
+    
+    % plain text description of database
+    if isfield(params,'label')
+        data.metadata.description = params.label;
+    end
     
     % grid parameters
     data.metadata.type = 'grid';
-    data.metadata.grid.beginAtMM = [p.xs(1) p.ys(1) p.zs(1)];
-    data.metadata.grid.separationMM = p.imsep * ones(1,3);
+    data.metadata.grid.beginAtMM = [params.p.xs(1) params.p.ys(1) params.p.zs(1)];
+    data.metadata.grid.separationMM = params.p.imsep * ones(1,3);
     if is2d
-        p.imsep(3) = 0;
+        params.p.imsep(3) = 0;
     end
-    data.metadata.grid.size = [length(p.xs) length(p.ys) length(p.zs)];
-    
-    % camera properties
-    data.metadata.camera.name = 'gantry';
-    data.metadata.camera.resolution = p.imsz([2 1]);
-    data.metadata.camera.isPanoramic = 1;
+    data.metadata.grid.size = [length(params.p.xs) length(params.p.ys) length(params.p.zs)];
     
     % gantry-specific properties
-    if isfield(p,'lim')
-        data.metadata.gantry.innerLimits = p.lim(:)';
+    if isfield(params.p,'lim')
+        data.metadata.gantry.innerLimits = params.p.lim(:)';
     end
-    data.metadata.gantry.offset = p.offset(:)';
-    if isfield(p,'zclear')
-        data.metadata.gantry.zClearance = p.zclear;
+    data.metadata.gantry.offset = params.p.offset(:)';
+    if isfield(params.p,'zclear')
+        data.metadata.gantry.zClearance = params.p.zclear;
     end
-    if isfield(p,'zoffs')
-        data.metadata.gantry.baseHeight = p.zoffs;
+    if isfield(params.p,'zclearht')
+        data.metadata.gantry.zClearance = params.p.zclearht;
     end
-    if isfield(p,'objgridac')
-        data.metadata.gantry.objectGridSpacing = p.objgridac;
+    if isfield(params.p,'zoffs')
+        data.metadata.gantry.baseHeight = params.p.zoffs;
     end
-    if isfield(p,'headclear')
-        data.metadata.gantry.headClearance = p.headclear;
+    if isfield(params.p,'objgridac')
+        data.metadata.gantry.objectGridSpacing = params.p.objgridac;
     end
-    if isfield(p,'arenafn')
-        data.metadata.gantry.arenaName = char(p.arenafn);
+    if isfield(params.p,'headclear')
+        data.metadata.gantry.headClearance = params.p.headclear;
+    end
+    if isfield(params.p,'arenafn')
+        data.metadata.gantry.arenaName = char(matfileremext(params.p.arenafn));
     else
         data.metadata.gantry.arenaName = '';
     end
-    if isfield(p,'maxA')
-        data.metadata.gantry.maximumAcceleration = p.maxA(:)';
+    if isfield(params.p,'maxA')
+        data.metadata.gantry.maximumAcceleration = params.p.maxA(:)';
     end
-    if isfield(p,'maxV')
-        data.metadata.gantry.maximumVelocity = p.maxV(:)';
+    if isfield(params.p,'maxV')
+        data.metadata.gantry.maximumVelocity = params.p.maxV(:)';
     end
     
     % other properties
-    data.metadata.needsUnwrapping = 1;
-    data.metadata.isGreyscale = double(p.imsz(3) > 1);
-    
-    % write metadata file
-    YAML.write('database_metadata.yaml',data);
-    clear data
+    data.metadata.needsUnwrapping = 0;
+    data.metadata.isGreyscale = double(size(params.p.imsz==2 || params.p.imsz(3)==1);
     
     % write entries file
     fid = fopen('database_entries.csv','w');
     fprintf(fid,'X [mm], Y [mm], Z [mm], Heading [degrees], Filename, Grid X, Grid Y, Grid Z\n');
-    for zi = 1:length(p.zs)
-        for yi = 1:length(p.ys)
-            for xi = 1:length(p.xs)
+    imsz = [];
+    for zi = 1:length(params.p.zs)
+        for yi = 1:length(params.p.ys)
+            for xi = 1:length(params.p.xs)
                 fname = sprintf('im_%03d_%03d_%03d.png',xi,yi,zi);
                 if exist(fname,'file')
-                    fprintf(fid, '%d, %d, %d, 0, %s, %d, %d, %d\n', p.xs(xi), p.ys(yi), p.zs(zi), fname, xi-1, yi-1, zi-1);
+                    fprintf(fid, '%d, %d, %d, 0, %s, %d, %d, %d\n', params.p.xs(xi), params.p.ys(yi), params.p.zs(zi), fname, xi-1, yi-1, zi-1);
+                    if isempty(imsz)
+                        % read size from image, as p.imsz appears to be
+                        % unreliable
+                        imsz = size(imread(fname));
+                        imsz = imsz([2 1]);
+                    end
                 end
             end
         end
     end
     fclose(fid);
+    
+    % camera properties
+    data.metadata.camera.name = 'gantry';
+    data.metadata.camera.resolution = imsz;
+    data.metadata.camera.isPanoramic = 1;
+    
+    % write metadata file
+    YAML.write('database_metadata.yaml',data);
+    clear data
     
     cd ..
 end
